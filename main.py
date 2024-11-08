4b16afcdcbbde7272a45b26cbff67db926c7fc12
alexis-gauluet <alexis.gauluet@edu.esiee.fr>
2024-11-08 12:51:45 +0000

    Permet de trouver la liste des nombres premiers de 1 à 100

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
+"""ajoute au code la fonction racine carré"""
 from math import sqrt
 
 #### Fonction secondaire
 
 
 def isprime(p):
+    """retourne un booléen. Si vrai le nombre passé en paramètre est premier"""
+    racine=int(sqrt(p))
+    prime=True
+    for n in range(2,racine+1):
+        if p%n==0:
+            prime=False
+    if p<=1:
+        prime=False
+    return prime
 
-    # votre code ici
 
-    pass
 
 #### Fonction principale
 
 
 def main():
-
+    """fonction principale du code"""
     # vos appels à la fonction secondaire ici
-
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
